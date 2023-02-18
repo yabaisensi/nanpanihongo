25e0fbeec1b253cb2179e024c6ffc93d055d5962
DESKTOP-2VQNNP9\administrator <demmy>
2023-02-19 00:52:32 +0900

    Saved on 周日 2023/02/19  0:52:32.16 by xu version 1

---
+++ commit.bat
@@ -0,0 +1,12 @@
+@echo off
+chcp 65001
+git log -20 | findstr version > temp.txt 
+set /p  temp=<temp.txt 
+set v_num=%temp:*version =%
+SET /a v_num+=1
+del temp.txt
+git add .
+git commit -m "Saved on %date% %time% by xu version %v_num%"
+git push
+git lg1
+pause
